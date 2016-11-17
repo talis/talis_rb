f41ebaae0ccfc57342f9241e31fea94694c0169c
Ross Singer <rossfsinger@gmail.com>
2016-11-17 11:36:35 -0500

    Docblocks

--- lib/talis/hierarchy/resource.rb
+++ lib/talis/hierarchy/resource.rb
@@ -14,24 +14,37 @@ module Talis
       @new_resource = false
       @deleted = false
 
+      # Manages the current & stored resource type so resources can be edited/
+      # deleted properly
+      # @param resource_type [String] the new type of the hierarchy resource
       def type=(resource_type)
         @original_type = type if persisted?
         @type = resource_type
       end
 
+      # Manages the current & stored resource id so resources can be edited/
+      # deleted properly
+      # @param resource_id [String] the new id of the hierarchy resource
       def id=(resource_id)
         @original_id = id if persisted?
         @id = resource_id
       end
 
+      # The resource type the API thinks the resource has (if it has been saved)
+      # @return [String]
       def stored_type
         persisted? ? @original_type || type : type
       end
 
+      # The resource id the API thinks the resource has (if it has been saved)
+      # @return [String]
       def stored_id
         persisted? ? @original_id || id : id
       end
 
+      # A boolean indicating if the resource exists in the remote Blueprint
+      # instance
+      # @return [Boolean]
       def persisted?
         !(@new_resource || @deleted)
       end
